fc08d52cbdd20f7a241fd36fd869a5cb65adc85d
Dani9898 <danieleoliva98@gmail.com>
2021-11-10 15:59:36 +0100

    es finito

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var app = new Vue(
+    {
+        el: "#container",
+        data: {
+            message: "Hello World",
+            immagine: "https://i.picsum.photos/id/612/200/300.jpg?hmac=vJ35AV5-TQa5ET5az0aESTnI3zaFCjRYD9OnYaiYIYc"
+        }
+    }
+)
